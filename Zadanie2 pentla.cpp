3f2658dabbb92af4cb04b8d61e220a6f85728ab6
AlicjaSiwczak <ala.siwczak@gmail.com>
2023-11-13 11:58:34 +0100

    Komentarz

---
+++ Zadanie2 pentla.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=1; i < 66666666; i++){
+        for(int i=1; i < 55555555; i++){
+            cout << " * " ;
+            cout << " * " ;
+        }
+        cout << endl;
+    }
+}
