8a22ce896d876f6a68584fedfbb1ff849c0d511f
AlicjaSiwczak <148056961+AlicjaSiwczak@users.noreply.github.com>
2024-04-25 09:27:58 +0200

    Add files via upload

---
+++ zadanie poprawione 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int tablica[26] = {2, 3, 5, 7, 11, 2, 3, 5, 7, 11, 31, 37, 41, 43, 47, 2, 3, 5, 7, 11, 31, 37, 41, 43, 47};
+	int szukane;
+    cout << "Podaj liczbe szukana: ";
+	cin >> szukane;
+	cout << "Poszukiwana liczba znajduje sie na pozycji: ";
+	for(int i = 0; i <= 25; i++){
+		if(tablica[i] == szukane){
+	   cout << i << ", ";	
+		}	
+	}
+}
